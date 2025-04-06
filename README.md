19e62502268180428bef61f22884e8323cf1cf7b
kiddersmxj <maxkiddmay@gmail.com>
2025-04-06 14:48:04 +0100

    intial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chronolog [![CMake](https://img.shields.io/github/actions/workflow/status/kiddersmxj/chronolog/cmake.yml?style=for-the-badge)](https://github.com/kiddersmxj/chronolog/actions/workflows/cmake.yml)
+
---
+++ src/chronolog.cpp
@@ -0,0 +1,63 @@
+#include "../inc/chronolog.hpp"
+
+int main(int argc, char** argv) {
+    InitConfig();
+    int HelpFlag = 0;
+    int VersionFlag = 0;
+    int opt;
+
+    // Get opt option defenitions
+    struct option Opts[] = {
+        { "help", no_argument, &HelpFlag, 1 },
+        { "version", no_argument, &VersionFlag, 1 },
+        { 0 }
+    };
+
+    // Infinite loop, to be broken when we are done parsing options
+    while (1) {
+        opt = getopt_long(argc, argv, "hv", Opts, 0);
+
+        // A return value of -1 indicates that there are no more options
+        if (opt == -1) {
+            if(HelpFlag && VersionFlag) {
+                Usage();;
+                return EXIT_FAILURE;
+            }
+            break;
+        } 
+
+        switch (opt) {
+        case 'h':
+            HelpFlag = 1;
+            break;
+        case 'v':
+            VersionFlag = 1;
+            break;
+        case '?':
+            Usage();
+            return EXIT_FAILURE;
+        default:
+            Usage();
+            break;
+        }
+    }
+
+    // Act on flags
+    if(HelpFlag) {
+        Usage();
+        return EXIT_SUCCESS;
+    }
+    if(VersionFlag) {
+        PrintVersion();
+        return EXIT_SUCCESS;
+    }
+
+    return 0;
+}
+
+// Copyright (c) 2024, Maxamilian Kidd-May
+// All rights reserved.
+
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree. 
+
